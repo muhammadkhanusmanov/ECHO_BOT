0430df20fd0a045c2a7a84949728d172c6be9f55
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-02-13 01:13:25 -0600

    init

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+import telegram
+import os
+
+token=os.environ['Token']
+
+bot = telegram.Bot(token)
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['POST'])
+def api():
+    data=request.get_json(force=True)
+    output = {'message':'Hello World'}
+    chat_id= 5032606464
+    bot.sendMessage(chat_id,text=output)
+    return jsonify(output)
+
+if __name__=='__main__':
+    app.run(debug=True, port=8021)
